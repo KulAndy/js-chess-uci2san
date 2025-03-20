910c61d28ddcc6898d4e0e2d81c9b2b8f2da62ab
Andrzej Kulesza <andykrk22@gmail.com>
2025-03-20 14:50:40 +0100

    change formatting

--- main.cpp
+++ main.cpp
@@ -6,34 +6,38 @@
 
 using namespace chess;
 
-std::string convert_uci_to_pgn(std::vector<std::string> uci_moves) {
+std::string convert_uci_to_pgn(std::vector<std::string> uci_moves)
+{
     Board board = Board::fromFen(constants::STARTPOS);
     std::vector<std::string> san_moves;
 
-    for (const auto& uci : uci_moves) {
+    for (const auto &uci : uci_moves)
+    {
         const Move move = uci::uciToMove(board, uci);
-        if (move == Move::NO_MOVE) {
+        if (move == Move::NO_MOVE)
+        {
             break;
         }
         san_moves.push_back(uci::moveToSan(board, move));
         board.makeMove(move);
     }
 
-    std::string pgn = "1.";
-    int move_number = 1;
-    for (size_t i = 0; i < san_moves.size(); i++) {
-        if (i % 2 == 0 && i != 0) {
-            move_number++;
-            pgn += " " + std::to_string(move_number) + ".";
+    std::string pgn = "";
+    for (size_t i = 0; i < san_moves.size(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            pgn += std::to_string(i / 2 + 1) + ".";
         }
-        pgn += " " + san_moves[i];
+        pgn += san_moves[i] + " ";
     }
 
     return pgn;
 }
 
 // 📌 Rejestracja std::vector<std::string> dla WebAssembly
-EMSCRIPTEN_BINDINGS(chess_module) {
+EMSCRIPTEN_BINDINGS(chess_module)
+{
     emscripten::register_vector<std::string>("VectorString");
     emscripten::function("convertUciToPgn", &convert_uci_to_pgn);
 }
